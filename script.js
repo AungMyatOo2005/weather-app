e263b35a757580edb47150ab52c59c1e09f42d9f
aung-myat-oo <aungmyatoo2025@gmail.com>
2023-11-11 16:22:41 +0630

    push

---
+++ script.js
@@ -0,0 +1,60 @@
+const key = "c44a49f7ef68185074cb2a588c0bb64a";
+const search = document.getElementById("search");
+const form = document.querySelector("form");
+const weatherParent=document.getElementById("weather")
+async function getData(cityName) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?unique=metrixc&q=${cityName}`;
+  const res = await fetch(url + `&appid=${key}`);
+  const resData = await res.json();
+  console.log(resData);
+  weather(resData);
+}
+function weather(data) {
+  function getImg() {
+    if (data.weather[0].main == "Rain") {
+      return "./images/rain.png";
+    } else if (data.weather[0].main == "Clouds") {
+      return "./images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      return "./images/clear.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      return "./images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      return "./images/mist.png";
+    }
+  }
+  const weatherEl= `
+  
+      <div class="weather">
+        <img src=${getImg()} alt="" class="weather-icon" />
+        <h1 class="temp">${Math.round(data.main.temp)}°C</h1>
+        <h2 class="city">${data.name}</h2>
+        <div class="detail">
+          <div class="col">
+          <img src="./images/humidity.png" alt="" />
+          <div>
+          <p class="humidity">${data.main.humidity}%</p>
+          <p>Humidity</p>
+            </div>
+          </div>
+          <div class="col">
+          <img src="./images/wind.png" alt="" />
+          <div>
+          <p class="wind">${data.wind.speed}km/h</p>
+          <p>Wind speed</p>
+          </div>
+          </div>
+          </div>
+          </div>
+          `;
+  weatherParent.innerHTML=weatherEl;
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  cityName = search.value.trim();
+  if (cityName.length > 0) {
+    getData(cityName);
+    weatherParent.innerHTML=""
+    search.value=""
+  }
+});
