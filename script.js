de85078530a891946e375d47524565f522ea4388
aung-myat-oo <aungmyatoo2025@gmail.com>
2023-11-12 22:06:37 +0630

    push

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const key = "c44a49f7ef68185074cb2a588c0bb64a";
 const search = document.getElementById("search");
 const form = document.querySelector("form");
-const weatherParent=document.getElementById("weather")
+const weatherParent = document.getElementById("weather");
 async function getData(cityName) {
   const url = `https://api.openweathermap.org/data/2.5/weather?unique=metrixc&q=${cityName}`;
-  const res = await fetch(url + `&appid=${key}`);
-  const resData = await res.json();
-  console.log(resData);
-  weather(resData);
+  try {
+    const res = await fetch(url + `&appid=${key}`);
+    if(res.ok){
+      const resData = await res.json();
+      weather(resData);
+      console.log(resData)
+    }else{
+      errorCity(cityName)
+      console.log("error at city name")
+    }
+  } catch (err) {
+    console.log(err.message);
+    failFetch()
+  }
 }
 function weather(data) {
   function getImg() {
@@ -23,7 +33,7 @@ function weather(data) {
       return "./images/mist.png";
     }
   }
-  const weatherEl= `
+  const weatherEl = `
   
       <div class="weather">
         <img src=${getImg()} alt="" class="weather-icon" />
@@ -47,14 +57,22 @@ function weather(data) {
           </div>
           </div>
           `;
-  weatherParent.innerHTML=weatherEl;
+  weatherParent.innerHTML = weatherEl;
 }
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   cityName = search.value.trim();
   if (cityName.length > 0) {
     getData(cityName);
-    weatherParent.innerHTML=""
-    search.value=""
+    weatherParent.innerHTML = "";
+    search.value = "";
   }
 });
+
+
+function errorCity(city){
+  weatherParent.innerHTML=`<h1 class="error">${city} is not found</h1>`
+}
+function failFetch(){
+  weatherParent.innerHTML = `<h1 class="error">Fail to Fetch</h1>`;
+}
